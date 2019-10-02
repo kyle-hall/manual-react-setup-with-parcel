236d813f467d5f76659c4226e1d2025e8585e27b
Kyle Hall <hallkd47@gmail.com>
2019-10-02 08:35:48 -0500

    fix vulnerabilities and remove sass imports since they still don't work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Header from './components/header'
 
-import './scss/app.scss'
-
 const HelloMessage = props => (
   <div>
     <Header />
-    <div className='container'>
+    <div className="container">
       <h1>Hello {props.name}</h1>
     </div>
   </div>
